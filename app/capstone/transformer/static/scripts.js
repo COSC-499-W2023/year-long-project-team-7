ac7b7c2966f72c61ffbd0afaba4a0b5f268e1579
Patch <patchfact@gmail.com>
2023-10-18 10:12:12 -0700

    cleaning up

--- app/capstone/transformer/static/scripts.js
+++ app/capstone/transformer/static/scripts.js
@@ -1,15 +1,1 @@
-// JavaScript to trigger the animation
-// document.addEventListener("DOMContentLoaded", function () {
-//     const welcomeText = document.getElementById("welcome-text");
-//     setTimeout(function () {
-//         welcomeText.style.color = "red";
-//     }, 3000); // Adjust the delay in milliseconds as needed
-// });
-$(document).ready(function () {
-    console.log("HELLO WORLD");
-});
-$(document).ready(function () {
-    $("#btnSubmit").click(function () {
-        alert("button");
-    });
-});
+
