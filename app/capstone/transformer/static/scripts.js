3823b89ad6e6928591773655d198991fa4afe8c9
Patch <patchfact@gmail.com>
2023-10-17 18:09:08 -0700

    trying to get scripts to work

---
+++ app/capstone/transformer/static/scripts.js
@@ -0,0 +1,11 @@
+// JavaScript to trigger the animation
+// document.addEventListener("DOMContentLoaded", function () {
+//     const welcomeText = document.getElementById("welcome-text");
+//     setTimeout(function () {
+//         welcomeText.style.color = "red";
+//     }, 3000); // Adjust the delay in milliseconds as needed
+// });
+
+function myFunc() {
+    Window.alert("Hello from me");
+}
