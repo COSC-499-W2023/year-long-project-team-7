6f6fd7707bf7157371e122daf801777e742643e1
Ferdinand <ferdinandhaaben@proton.me>
2023-10-18 18:05:44 -0700

    test out mypy

--- app/capstone/transformer/views.py
+++ app/capstone/transformer/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render, redirect
-from .forms import  TransformerForm
-from .models import *
+from django.http import HttpRequest, HttpResponse
+from .forms import TransformerForm
+from .models import Conversion, File
+from typing import List, Dict
 import json
 from .generator import *
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     return render(request, 'index.html')
 
-def transform(request):
+def transform(request: HttpRequest) -> HttpResponse:
+
     if request.method == 'POST':
-        form = TransformerForm(request.POST)
+
+        form: TransformerForm = TransformerForm(request.POST)
+
         if form.is_valid():
-            conversion = Conversion()
+            conversion: Conversion = Conversion()
             user_params = {
                 'text_input': form.cleaned_data['text_input'],
                 'language': form.cleaned_data['language'],
@@ -21,32 +26,31 @@ def transform(request):
             conversion.user_parameters = json.dumps(user_params)
             conversion.save()
 
-            files = []
+            files: List[File] = []
         
             for uploaded_file in request.FILES.getlist('files'):
-                new_file = File()
+                new_file: File = File()
                 new_file.user = request.user if request.user.is_authenticated else None
                 new_file.conversion = conversion
-                new_file.type = uploaded_file.content_type
+                if uploaded_file.content_type is not None:  # Add this check
+                    new_file.type = uploaded_file.content_type
                 new_file.file = uploaded_file
                 new_file.save()
                 files.append(new_file)
             try:
-                result = generate_output(files, conversion)
+                result: Dict = generate_output(files, conversion)
             except:
                 pass
 
-            return redirect('results')
-    else:
-        form = TransformerForm()
+            return redirect('results')        
 
-    return render(request, 'transform.html', {'form': form})
+    return render(request, 'transform.html', {'form': TransformerForm()})
 
-def results(request):
+def results(request: HttpRequest) -> HttpResponse:
     return render(request, 'results.html')
 
-def home(request):
+def home(request: HttpRequest) -> HttpResponse:
     return render(request, 'home.html')
 
-def about(request):
+def about(request: HttpRequest) -> HttpResponse:
     return render(request, 'about.html')
