276af45689c966cb39840f07e6c6902d09092b77
liamras <77363708+liamras@users.noreply.github.com>
2023-10-22 17:30:31 -0700

    Update urls.py

--- app/capstone/transformer/urls.py
+++ app/capstone/transformer/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path("transform", views.transform, name="transform"),
     path("home", views.home, name='home'),
     path("about", views.about, name='about'),
-    path("results", views.results, name='results')
+    path("results", views.results, name='results'),
+    path('signup', views.signup, name="signup"),
+    path('signin', views.signin, name="signin"),
+    path('signout', views.signout, name="signout"),
 ]
