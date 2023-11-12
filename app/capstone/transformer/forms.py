25ab119201cb599703abefa514d12182342a814f
liamras <77363708+liamras@users.noreply.github.com>
2023-11-12 09:44:19 -0800

    Update forms.py with signin/signup

--- app/capstone/transformer/forms.py
+++ app/capstone/transformer/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.forms import TextInput, Select
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class TransformerForm(forms.Form):
@@ -18,3 +20,20 @@ class TransformerForm(forms.Form):
             attrs={"type": "range", "min": 0, "max": 100, "class": "custom-slider"}
         )
     )
+
+
+class SignUpForm(forms.Form):
+    first_name = forms.CharField(max_length=30, required=True, help_text="Required.")
+    last_name = forms.CharField(max_length=30, required=True, help_text="Required.")
+    email = forms.EmailField(
+        max_length=254,
+        required=True,
+        help_text="Required. Enter a valid email address.",
+    )
+    model = User
+    fields = ("username", "first_name", "last_name", "email", "password1", "password2")
+
+
+class SignInForm(forms.Form):
+    username = forms.CharField(max_length=150, required=True)
+    password = forms.CharField(widget=forms.PasswordInput, required=True)
