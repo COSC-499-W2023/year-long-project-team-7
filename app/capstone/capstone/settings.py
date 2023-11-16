b8c64b8dec5dab245f95ae132022bd7186da1ef5
liamras <77363708+liamras@users.noreply.github.com>
2023-11-15 16:39:26 -0800

    Update settings.py

--- app/capstone/capstone/settings.py
+++ app/capstone/capstone/settings.py
@@ -13,6 +13,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+# Loading environment variables using dotenv in Python
+from dotenv import load_dotenv
+
+# Load the environment variables from the .env file
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
