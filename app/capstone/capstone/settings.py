5718e7774ab7abad3213e0c557e28cae26be307c
liamras <77363708+liamras@users.noreply.github.com>
2023-11-26 18:19:06 -0800

    Update settings.py
    Added emailing settings

--- app/capstone/capstone/settings.py
+++ app/capstone/capstone/settings.py
@@ -140,3 +140,14 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+
+# Email settings
+EMAIL_BACKEND =  'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_FROM = 'platonix.ai@gmail.com'
+EMAIL_HOST_USER = 'platonix.ai@gmail.com'
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+PASSWORD_RESET_TIMEOUT = 14400
