b40309b49c5add161b5c2d37169dbcce2a14b1a7
liamras <77363708+liamras@users.noreply.github.com>
2023-11-26 18:24:30 -0800

    Update views.py
    Implimented email activation

--- app/capstone/transformer/views.py
+++ app/capstone/transformer/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib import messages
 from django.http import HttpRequest, HttpResponse, HttpResponseForbidden
+from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.core.mail import EmailMessage
 from .forms import TransformerForm
 from .forms import SignUpForm
 from .forms import SignInForm
 from .models import Conversion, File
+from .tokens import account_activation_token
 from typing import List, Dict
 import json
 from .generator import generate_output
@@ -84,14 +90,50 @@ def signup(request: HttpRequest) -> HttpResponse:
     if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            messages.success(request, "Account successfully created.")
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            activateEmail(request, user, form.cleaned_data.get('email'))
             return redirect("signin")
         else:
             messages.error(request, "Error in the form submission.")
     else:
         form = SignUpForm()
     return render(request, "signup.html", {"form": form})
+
+def activate(request, uidb64, token):
+    User = get_user_model()
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except:
+        user = None
+    
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, "Email verified. You can now log into your account.")
+        return redirect('signin')
+    else:
+        messages.error(request, "Activation link is invalid!")
+    return redirect('index')
+
+def activateEmail(request, user, to_email):
+    mail_subject = "Activate your Platonix account"
+    message = render_to_string("template_activate_account.html", {
+        'user': user.username,
+        'domain': get_current_site(request).domain,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': account_activation_token.make_token(user),
+        "protocol": 'https' if request.is_secure() else 'http'
+    })
+    email = EmailMessage(mail_subject, message, to=[to_email])
+    if email.send():
+        messages.success(request, f'Hi {user}, please go to your inbox at {to_email} \
+                     and click on the activiation link to complete registration.')
+    else:
+        messages.error(request, f'Problem sending email to {to_email}. Please verify \
+                       spelling.')
 
 
 def signin(request: HttpRequest) -> HttpResponse:
