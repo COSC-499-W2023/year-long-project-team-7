72e2f0779672efa423ef63b94040fb11a69a83aa
Ferdinand <ferdinandhaaben@proton.me>
2023-12-17 10:05:23 -0800

    add debug env var

--- app/capstone/capstone/settings.py
+++ app/capstone/capstone/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG") == "True"
 
 ALLOWED_HOSTS = ["platonix.app", "www.platonix.app", "localhost", "127.0.0.1"]
 
