7aa8ff361d3e6726f038331cab30d0e30dd5871d
Ferdinand <ferdinandhaaben@proton.me>
2023-12-17 10:17:15 -0800

    absolute env path

--- app/capstone/capstone/wsgi.py
+++ app/capstone/capstone/wsgi.py
@@ -12,7 +12,9 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dotenv import load_dotenv
 
-load_dotenv()
+dotenv_path = os.path.join(os.path.dirname(__file__), "..", ".env")
+load_dotenv(dotenv_path=dotenv_path)
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "capstone.settings")
 
 application = get_wsgi_application()
