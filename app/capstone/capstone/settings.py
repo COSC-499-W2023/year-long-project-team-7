51443bee5337ceb007a2ee699cd26e14ccd652a5
Ferdinand <ferdinandhaaben@proton.me>
2023-12-17 10:23:00 -0800

    absolute env path

--- app/capstone/capstone/settings.py
+++ app/capstone/capstone/settings.py
@@ -12,10 +12,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv_path = os.path.join(BASE_DIR, ".env")
+load_dotenv(dotenv_path=dotenv_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
--- app/capstone/capstone/wsgi.py
+++ app/capstone/capstone/wsgi.py
@@ -10,10 +10,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dotenv import load_dotenv
 
-dotenv_path = os.path.join(os.path.dirname(__file__), "..", ".env")
-load_dotenv(dotenv_path=dotenv_path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "capstone.settings")
 
