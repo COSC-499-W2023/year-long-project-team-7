deb0b6645e86d0e07a532c8b0b72d9db7ab4fbf3
Ferdinand <ferdinandhaaben@proton.me>
2023-12-17 17:22:25 -0800

    update settings again

--- app/capstone/capstone/settings.py
+++ app/capstone/capstone/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_KEY")
+SECRET_KEY = "django-insecure-dyb_8=*dg&-81#5leycizjybho@5un+4#4k2_ui+&tr)2-r(s$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DJANGO_DEBUG") == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = ["platonix.app", "www.platonix.app", "localhost", "127.0.0.1"]
 
@@ -76,9 +76,9 @@ WSGI_APPLICATION = "capstone.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DATABASE_NAME"),
+        "NAME": "dev",
         "USER": "postgres",
-        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "PASSWORD": "password",
         "HOST": "localhost",
         "PORT": "5432",
     }
