3d681cdfc11737572db97ec42df4cc76e9020abf
liamras <77363708+liamras@users.noreply.github.com>
2024-01-16 16:10:22 -0800

    Updated tests.py
    Tests are functional now. 
    
    TODO: fix views.py type annotation

--- app/capstone/transformer/tests.py
+++ app/capstone/transformer/tests.py
@@ -167,47 +167,67 @@ class UserSignInTestCase(TestCase):
         self.assertContains(response, "Incorrect Credentials.")
         self.assertFalse(response.wsgi_request.user.is_authenticated)
 
+
 class EmailVerificationTest(TestCase):
-    def setUp(self):
-        self.user = get_user_model().objects.create_user(
-            username='testuser',
-            email='testuser@example.com',
-            password='testpassword'
-        )
-        self.user.is_active = False
-    
     def test_email_verification_flow(self):
-        # create activation email
-        activation_url = reverse('activate', kwargs={'uidb64': urlsafe_base64_encode(force_bytes(self.user.pk)), 'token': account_activation_token.make_token(self.user)})
-        response = self.client.get(activation_url)
-        # Check that the email was sent
+        # Make a POST request to the sign-up view with valid data
+        response = self.client.post(
+            reverse("signup"),
+            {
+                "username": "testuser",
+                "email": "testuser@example.com",
+                "password1": "testpassword123",
+                "password2": "testpassword123",
+            },
+        )
+        # Check if the user was created and logged in successfully
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("signin"))
+        self.assertEqual(get_user_model().objects.count(), 1)
+        self.assertEqual(get_user_model().objects.first().username, "testuser")
+        self.user = get_user_model().objects.first()
+        # Check if an activation email was sent
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].subject, "Activate your Platonix account")
         # Grab activation link from email
-        activation_link_start = 'http://127.0.0.1:8000'
-        activation_link_end = reverse('activate', kwargs={
-            'uidb64': urlsafe_base64_encode(force_bytes(self.user.pk)).decode(),
-            'token': account_activation_token.make_token(self.user)
-        })
-        activation_link = (
-            mail.outbox[0].body
-            .split(activation_link_start)[-1]
-            .split(activation_link_end)[0]
+        activation_link_start = "http://testserver"
+        activation_link_end = reverse(
+            "activate",
+            kwargs={
+                "uidb64": urlsafe_base64_encode(force_bytes(self.user.pk)),
+                "token": account_activation_token.make_token(self.user),
+            },
         )
+        activation_link = activation_link_start + activation_link_end
         # Go to activation link
         response = self.client.get(activation_link)
         # Check that user is activated and redirected
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, reverse('signin'))
+        self.assertEqual(response.url, reverse("signin"))
         # Check for the active user
+        self.user.refresh_from_db()
         self.assertTrue(self.user.is_active)
-    
+
     def test_invalid_activation_link(self):
+        # Make a POST request to the sign-up view with valid data
+        response = self.client.post(
+            reverse("signup"),
+            {
+                "username": "testuser",
+                "email": "testuser@example.com",
+                "password1": "testpassword123",
+                "password2": "testpassword123",
+            },
+        )
+        self.user = get_user_model().objects.first()
         # Visit invalid activation link
-        invalid_activation_url = reverse('activate', kwargs={'uidb64': 'invalid', 'token': 'invalid'})
+        invalid_activation_url = reverse(
+            "activate", kwargs={"uidb64": "invalid", "token": "invalid"}
+        )
         response = self.client.get(invalid_activation_url)
         # Check that user is redirected to index page
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, reverse('index'))
+        self.assertEqual(response.url, reverse("index"))
         # Check that the user is not activated
+        self.user.refresh_from_db()
         self.assertFalse(self.user.is_active)
