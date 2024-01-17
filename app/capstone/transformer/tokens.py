330656912ffa4c5c0f7542728d0b761ccb6fed39
liamras <77363708+liamras@users.noreply.github.com>
2024-01-16 22:21:52 -0800

    Update tokens.py to type annotation

--- app/capstone/transformer/tokens.py
+++ app/capstone/transformer/tokens.py
@@ -1,10 +1,15 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.contrib.auth.models import AbstractBaseUser
 import six
 
+
 class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
-    def _make_hash_value(self, user, timestamp):
+    def _make_hash_value(self, user: AbstractBaseUser, timestamp: float) -> str:
         return (
-            six.text_type(user.pk) + six.text_type(timestamp) + six.text_type(user.is_active)
+            six.text_type(user.pk)
+            + six.text_type(timestamp)
+            + six.text_type(user.is_active)
         )
-    
+
+
 account_activation_token = AccountActivationTokenGenerator()
