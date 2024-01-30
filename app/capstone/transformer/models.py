3f702ae0e9d4f4a52ac50652aa8a406d1a829625
liamras <77363708+liamras@users.noreply.github.com>
2024-01-29 21:13:13 -0800

    Update models.py
    Added profile model

--- app/capstone/transformer/models.py
+++ app/capstone/transformer/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.db.models import JSONField
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Conversion(models.Model):
@@ -31,3 +32,23 @@ class Products(models.Model):
     get_display_price = models.FloatField()
     description = models.TextField()
     phrase = models.TextField()
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default="default.jpg", upload_to="profile_pics")
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
+
+    # Override the save method of the model
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)  # Open image
+
+        # resize image
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)  # Resize image
+            img.save(self.image.path)  # Save it again and override the larger image
