bdc0221e3208bc18fe572a795dc9a455551592bd
kmalott <kmalott2@gmail.com>
2024-02-01 18:18:37 -0800

    add delete subscription function

--- app/capstone/transformer/subscriptionManager.py
+++ app/capstone/transformer/subscriptionManager.py
@@ -1,7 +1,8 @@
 from .models import Subscription
 from datetime import date
+from django.contrib.auth.models import User
 
-def has_valid_subscription(user):
+def has_valid_subscription(user: User): 
     if hasattr(user, 'subscription'):
         subscription = user.subscription
         return (
@@ -13,9 +14,17 @@ def has_valid_subscription(user):
         )
     return False
 
-def give_subscription_to_user(user, start_date, end_date):
+def give_subscription_to_user(user: User, start_date, end_date): #type: ignore
     subscription, created = Subscription.objects.get_or_create(user=user)
     subscription.has_subscription = True
     subscription.start_date = start_date
     subscription.end_date = end_date
     subscription.save()
+
+def delete_subscription(user: User):
+    try:
+        subscription = Subscription.objects.get(user=user)
+        subscription.delete()
+        print(f"Subscription deleted for user {user}")
+    except Subscription.DoesNotExist:
+        print(f"No subscription found for user {user}")
