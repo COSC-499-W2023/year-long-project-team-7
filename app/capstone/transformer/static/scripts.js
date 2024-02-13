f18f5922d3e29d94426036bd999a0b974044308a
Patch <patchfact@gmail.com>
2024-02-12 16:46:30 -0800

    history page responsive with dropdown

--- app/capstone/transformer/static/scripts.js
+++ app/capstone/transformer/static/scripts.js
@@ -16,9 +16,7 @@ addEventListener("load", () => {
     }
 });
 
-
 $(document).ready(function () {
-
     $(".file-download-button").click(function () {
         const fileUrl = $(this).attr("data-fileurl");
         window.location.href = fileUrl;
