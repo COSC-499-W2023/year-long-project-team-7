2d6f7dfab5e654f26c29e870bc0746cdbe5bb49c
Ferdinand <ferdinandhaaben@proton.me>
2024-03-10 17:26:40 -0700

    bugfixes

--- app/capstone/transformer/templatetags/json_filter.py
+++
@@ -1,9 +0,0 @@
-from django import template
-import json
-
-register = template.Library()
-
-
-@register.filter
-def get_item(dictionary: str, key: str) -> str:
-    return str(json.loads(dictionary).get(key))
