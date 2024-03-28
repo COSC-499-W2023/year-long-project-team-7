94639d28bbc12f021247e5c1ec6b683fbafe67d1
Keiran Malott <kmalott2@gmail.com>
2024-03-28 15:59:41 -0700

    complete basic exercise generation

--- app/capstone/transformer/exerciseGenerator.py
+++ app/capstone/transformer/exerciseGenerator.py
@@ -1,12 +1,19 @@
 from .models import Exercise
-
+from .exerciseManager import (
+    FieldTypes,
+    ExerciseManager,
+    ExerciseContent,
+)
+from .utils import error
+from concurrent.futures import ThreadPoolExecutor
+from .prompts import *
+from .openAiManager import OpenAiExerciseManager
 
 class ExerciseGenerator:
     def __init__(self, input_file_text: str, exercise: Exercise):
-        #need to create exercise manager
-        #self.exercise_manager = ExerciseManager()
+        self.exercise_manager = ExerciseManager()
 
-        #need to add openAI manager with exercise inputs
+        self.openai_manager = OpenAiExerciseManager(input_file_text, exercise)
 
         self.exercise = exercise
 
@@ -14,4 +21,46 @@ class ExerciseGenerator:
         self.num_multiple_choice = exercise.num_multiple_choice
         self.num_short_ans = exercise.num_short_ans
         self.num_long_ans = exercise.num_long_ans
+        self.num_slides = exercise.num_true_false + exercise.num_multiple_choice + exercise.num_short_ans + exercise.num_long_ans
         self.template = exercise.template
+
+    def build_slide(self, slide_num: int) -> ExerciseContent:
+        layout = self.exercise_manager.get_content_slide_layout()
+
+        fields = self.exercise_manager.get_slide_layout_fields(layout)
+
+        exercise_content = ExerciseContent(slide_num, layout, fields)
+
+        exercise_json = exercise_content.to_json_string()
+
+        response = self.openai_manager.prompt_chat(EXERCISE.format(slide_json=exercise_json))
+
+        try:
+            exercise_content.update_from_json(response)
+        except Exception as e:
+            error(e)
+
+        return exercise_content
+
+    def build_presentation(self) -> str:
+        if self.template is not None:
+            self.exercise_manager.setup(self.template)
+
+        slide_contents: list[ExerciseContent] = []
+
+        # Fetch slide content in parallel for speed
+        with ThreadPoolExecutor() as executor:
+            future_slides = executor.map(
+                self.build_slide, range(1, self.num_slides + 1)
+            )
+            slide_contents = list(future_slides)
+
+        # Sort and add slides to presentation
+        sorted_slide_contents = sorted(
+            slide_contents, key=lambda slide: slide.slide_num
+        )
+
+        for slide_content in sorted_slide_contents:
+            self.exercise_manager.add_slide_to_presentation(slide_content)
+
+        return self.exercise_manager.save_presentation(self.exercise.id)
--- app/capstone/transformer/openAiManager.py
+++ app/capstone/transformer/openAiManager.py
@@ -1,7 +1,7 @@
 import typing
 from openai import OpenAI
 from django.conf import settings
-from .models import Conversion, File
+from .models import Conversion, File, Exercise
 from tenacity import retry, stop_after_attempt, wait_random_exponential
 import re
 from .prompts import *
@@ -26,6 +26,63 @@ class OpenAiManager:
             language=conversion.language,
             complexity=conversion.complexity,
             prompt=conversion.prompt,
+            input_file_text=input_file_text,
+        )
+        self.messages = [{"role": "system", "content": self.instructions}]
+
+    def prompt_chat(self, prompt: str) -> str:
+        self.messages.append({"role": "user", "content": prompt})
+        return self.create_chat()
+
+    # REQUESTS WITH EXPONENTIAL BACKOFF
+    # All requests need to be done like this to deal with rate-limiting
+    @retry(wait=wait_random_exponential(min=1, max=400), stop=stop_after_attempt(100))
+    def create_chat(self) -> str:
+        return str(
+            self.client.chat.completions.create(  # type: ignore
+                model=self.model,
+                messages=self.messages,
+                response_format={"type": "json_object"},
+            )
+            .choices[0]
+            .message.content
+        )
+
+    @retry(wait=wait_random_exponential(min=1, max=400), stop=stop_after_attempt(100))
+    def list_files(self):  # type: ignore
+        return self.client.files.list().data
+
+    @retry(wait=wait_random_exponential(min=1, max=400), stop=stop_after_attempt(100))
+    def delete_file(self, file_id: str) -> None:
+        self.client.files.delete(file_id)
+
+    @retry(wait=wait_random_exponential(min=1, max=400), stop=stop_after_attempt(100))
+    def list_assistants(self):  # type: ignore
+        return self.client.beta.assistants.list(limit=100).data
+
+    @retry(wait=wait_random_exponential(min=1, max=400), stop=stop_after_attempt(100))
+    def delete_assistant(self, assistant_id: str) -> None:
+        self.client.beta.assistants.delete(assistant_id)
+
+
+class OpenAiExerciseManager:
+    def __init__(self, input_file_text: str, exercise: Exercise) -> None:
+        self.client = OpenAI(api_key=settings.OPENAI_API_KEY, organization="org-hYPJO2WKqFAN1o75AdfdfeBx")  # type: ignore
+
+        self.model = exercise.model
+
+        all_files = self.list_files()
+        all_assistants = self.list_assistants()
+        for file in all_files:
+            self.delete_file(file.id)
+
+        for assistant in all_assistants:
+            self.delete_assistant(assistant.id)
+
+        self.instructions = SYSTEM_PROMPT.format(
+            language=exercise.language,
+            complexity=exercise.complexity,
+            prompt=exercise.prompt,
             input_file_text=input_file_text,
         )
         self.messages = [{"role": "system", "content": self.instructions}]
